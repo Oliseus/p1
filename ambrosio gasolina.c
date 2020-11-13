33658373927a24262a9146833e3803ba0aab146e
Oliseus <60153304+Oliseus@users.noreply.github.com>
2020-11-13 13:50:07 -0300

    Update ambrosio gasolina.c

--- ambrosio gasolina.c
+++ ambrosio gasolina.c
@@ -8,10 +8,11 @@ int main(){
     }
     else if(l*10>=d/(p+1)){ // formula para garantir se o carro consegue chegar a um posto de combustivel, vc pode ter o dinheiro que for, mas se o combustivel n durar até o posto n vai adiantar de nada
         d-=l*10;
-        r-=g*(d/10); r++; // r++ é equivalente a r = r + 1, isso é para ajustar a quantidade de litros, ex: para d = 15, d/10 = 1, so q 1 litro n é suficiente então boto 1 litro a mais 
+        l = d/10; l++; // l++ é equivalente a l = l + 1, isso é para ajustar a quantidade de litros que tem que comprar, ex: para d = 15, d/10 = 1, so q 1 litro n é suficiente então boto 1 litro a mais
         if(d%10==0){
-            r--;    // r-- é equivalente a r = r - 1, caso a divisao for exata então não era pra eu ter somado 1 litro entao eu ajusto de volta
+            l--;    // l-- é equivalente a l = l - 1, caso a divisao for exata então não era pra eu ter somado 1 litro entao eu ajusto de volta
         }
+        r-=g*(d/10);
         if(r<0){ // 
             puts("Nao pode viajar."); // puts é um printf mais simples
         }
